80d14536190d33c3c02a29541c060a2cc10a816b
ujjwalarvind.lade <ujjwalarvind.lade@ivycomptech.com>
2023-04-24 14:07:08 +0530

    string change

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 mongoose
-  .connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.DB_URL)
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((error) => console.error("Error connecting to MongoDB Atlas:", error));
 
